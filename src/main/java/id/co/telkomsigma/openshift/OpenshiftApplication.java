9fe1d3ee42b192c4da3f9a2118059bd477010672
rudi sadria <rudi.sadria@gmail.com>
2019-08-14 11:39:43 +0700

    asdad

--- src/main/java/id/co/telkomsigma/openshift/OpenshiftApplication.java
+++ src/main/java/id/co/telkomsigma/openshift/OpenshiftApplication.java
@@ -2,12 +2,19 @@ package id.co.telkomsigma.openshift;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class OpenshiftApplication {
+public class OpenshiftApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OpenshiftApplication.class, args);
 	}
+	
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(OpenshiftApplication.class);
+    }
 
 }
