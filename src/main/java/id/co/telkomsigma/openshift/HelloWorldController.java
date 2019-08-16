a4d839df28b7f0182edbea4bd3ac529286b763fa
rudi sadria <rudi.sadria@gmail.com>
2019-08-16 15:06:47 +0700

    fadhil jahat

--- src/main/java/id/co/telkomsigma/openshift/HelloWorldController.java
+++ src/main/java/id/co/telkomsigma/openshift/HelloWorldController.java
@@ -1,14 +1,18 @@
 package id.co.telkomsigma.openshift;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
+	@Value("${name}")
+	private String name = "";
+	
 	@RequestMapping("/")
 	public String helloWorld() {
-		return "Hello World";
+		return "Hello World "+name;
 	}
 	
 	@RequestMapping("/hello")
